ec7ea71702236e99912ab2a7523624c63236f9dd
Mads Nedergaard <nedergaardmads@gmail.com>
2018-05-22 22:26:35 +0200

    added index for testing

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+$msg ="Yo, it's live<span style='color: #666;'>" . date('Y-m-d') . '</span>';
+echo "<body style='flex-direction: column; background: #333; color: #fff; font-family: sans-serif; text-align: center; font-size: 5rem; display: flex; justify-content: center; align-items: center;'>$msg</body>";
