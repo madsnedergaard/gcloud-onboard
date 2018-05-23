594672125dda3a58f2b7b0c47dd39ba18d7e723b
Mads Nedergaard <nedergaardmads@gmail.com>
2018-05-23 07:59:07 +0200

    Updated PHP version and output it on page

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php 
-$msg ="Yo, it's live now<span style='color: #666;'>" . date('Y-m-d') . '</span><div>Update #1</div>';
+$msg ="Yo, it's live now<span style='color: #666;'>" . date('Y-m-d') . '</span><div>PHP is '. phpversion() .'</div>';
 echo "<body style='flex-direction: column; background: #333; color: #fff; font-family: sans-serif; text-align: center; font-size: 5rem; display: flex; justify-content: center; align-items: center;'>$msg</body>";
